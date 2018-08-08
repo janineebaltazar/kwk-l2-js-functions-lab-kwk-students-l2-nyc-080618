2b10f1278d58c21f74e5f125bfff492657d7dad4
janineebaltazar <janine.ebaltazar@gmail.com>
2018-08-08 18:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(numBlocks){
+  if ( numBlocks > 42){
+    return numBlocks - 42;
+  }
   var result = 42 - numBlocks;
   return result;
 }
